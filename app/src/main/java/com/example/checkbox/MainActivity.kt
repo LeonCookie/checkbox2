1e82805401f3e7500058f258c5d09dd89ea06304
LeonCookie <leongdynia1105@gmail.com>
2021-10-11 15:15:27 +0200

    skonczone w MainActivity.kt

--- app/src/main/java/com/example/checkbox/MainActivity.kt
+++ app/src/main/java/com/example/checkbox/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +20,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)// s
         setContentView(R.layout.activity_main)
+        title="lista"
+        mleko=findViewById(R.id.checkBox3)
+        jajka=findViewById(R.id.checkBox)
+        kawa=findViewById(R.id.checkBox2)
+        button=findViewById(R.id.button)
+        button.setOnClickListener {
+            var totalAmount: Int = 0
+            val result = StringBuilder()
+            result.append("Wybrane przedmioty")
+            if(mleko.isChecked){
+                result.append("\nMleko 50")
+                totalAmount += 50
+            }
+            if(jajka.isChecked){
+                result.append("\njajka 75")
+                totalAmount += 75
+            }
+            if(kawa.isChecked){
+                result.append("\nKawa 100")
+                totalAmount += 100
+            }
+            result.append("\nTotal: " + totalAmount + "Rs")
+            Toast.makeText(applicationContext, result.toString(), Toast.LENGTH_SHORT).show()
+        }
+
     }
 }//s
