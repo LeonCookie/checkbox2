f41df772d2aa1f1e622b8c031a09b530480d2b4b
LeonCookie <leongdynia1105@gmail.com>
2021-10-11 15:00:30 +0200

    pierwsze

---
+++ app/src/main/java/com/example/checkbox/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.checkbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)// s
+        setContentView(R.layout.activity_main)
+    }
+}
