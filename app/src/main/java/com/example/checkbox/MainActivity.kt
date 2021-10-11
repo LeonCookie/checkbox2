863bb6c5c7889a410eb0933168ef5df3d367d230
LeonCookie <leongdynia1105@gmail.com>
2021-10-11 15:08:47 +0200

    przycis i lateinit var

--- app/src/main/java/com/example/checkbox/MainActivity.kt
+++ app/src/main/java/com/example/checkbox/MainActivity.kt
@@ -2,8 +2,20 @@ package com.example.checkbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 
 class MainActivity : AppCompatActivity() {
+
+    //vary
+    lateinit var mleko: CheckBox
+    lateinit var jajka: CheckBox
+    lateinit var kawa: CheckBox
+    lateinit var button: Button
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)// s
         setContentView(R.layout.activity_main)
